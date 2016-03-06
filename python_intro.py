e760af622c40c2c14a6963810d5e34235e14ec64
mirtlbu <bubuzoglo@gmail.com>
2016-03-06 12:49:13 +0300

    first

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Mirtl']
+def hi(name):
+    print('Hi ' + name + '!')
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1, 6):
+    print(i)
